19774d53823d111e67e977dd3231e36cdfc6b379
Surya <suryaparkerstarc@gmail.com>
2026-03-26 23:11:45 +0530

    fresh starttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://six0-og6j.onrender.com",
-      "https://front-sand-pi.vercel.app/"
+      "https://front-ten-sable.vercel.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
